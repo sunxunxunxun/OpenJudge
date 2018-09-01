d308ac5bea66df426ccc9408b054561faadf4899
sunxunxunxun <34117516+sunxunxunxun@users.noreply.github.com>
2018-09-01 12:04:53 +0800

    Update LongestCommonSubseq.cpp

--- DP/longest-common-subsequence/LongestCommonSubseq.cpp
+++ DP/longest-common-subsequence/LongestCommonSubseq.cpp
@@ -11,7 +11,7 @@ int main()
 {
     while(scanf("%s%s", s1, s2) !=EOF){
         int l1 = strlen(s1), l2 = strlen(s2);
-        int dp[l1+1][l2+1];//
+        int dp[l1+1][l2+1];  // dp[i][j]表示s1第i个字符到s2第j个字符为止最长公共序列长度
         memset(dp, 0, sizeof(dp));
         for(int i=1; i<=l1; i++)
             for(int j=1; j<=l2; j++){
