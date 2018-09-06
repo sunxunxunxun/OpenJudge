297fa515f51edf91114b568d1cbc30db8937cc39
sunxunxunxun <34117516+sunxunxunxun@users.noreply.github.com>
2018-09-06 17:42:29 +0800

    [dp] difficultu *** && importance****
    不是很理解。背包问题变种

---
+++ DP/combinatorial-number/CombinatorialNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+int f[1210];
+int num[1210];
+int main()
+{
+    int n,t;
+    scanf("%d%d",&n,&t);
+    memset(f,0,sizeof(f));
+    f[0] = 1;
+    for(int i = 1 ; i <= n ; i ++)
+        scanf("%d",&num[i]);
+    for(int i = 1 ; i <= n ; i ++)
+        for(int j = t ; j >= num[i] ; j --)
+            f[j] += f[j-num[i]];
+    printf("%d\n",f[t]);
+    return 0;
+}
