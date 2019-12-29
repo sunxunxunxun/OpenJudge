f522e8e59ba1685550fa9f673a0d69d74bce7363
sunxunxunxun <34117516+sunxunxunxun@users.noreply.github.com>
2019-12-29 14:39:18 +0800

    Create Date.cpp

---
+++ Lutece/Date.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+using namespace std;
+int a,b;
+
+int main()
+{
+    while(scanf("%d %d",&a,&b)!=EOF&&(a||b)){
+        int cnt =0;
+        cnt =b/3-(a-1)/3;
+        printf("%d\n",cnt);
+    }
+    return 0;
+}
